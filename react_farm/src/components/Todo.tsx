ee6d3e2ecfef8a609a8eb949cabfdd0306952155
ot07 <ot07neoki@gmail.com>
2022-11-27 03:39:01 +0900

    add fetching Todo function

--- react_farm/src/components/Todo.tsx
+++ react_farm/src/components/Todo.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { LogoutIcon } from '@heroicons/react/outline'
 import { useProcessAuth } from '../hooks/useProcessAuth'
+import { useQueryUser } from '../hooks/useQueryUser'
+import { useQueryTasks } from '../hooks/useQueryTasks'
 
 export const Todo = () => {
   const { logout } = useProcessAuth()
+  const { data: dataUser } = useQueryUser()
+  const { data: dataTasks, isLoading: isLoadingTasks } = useQueryTasks()
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center font-mono text-gray-600">
